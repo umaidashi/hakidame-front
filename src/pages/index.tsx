7524660234b5cb9a78adf83338a4f67d7d02ef4c
umaidashi <umaidashi18@gmail.com>
2023-03-25 18:32:40 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,26 @@
-const SERVER_URL = process.env.SERVER_URL;
+import HakidameList from "@/components/pages/HakidameList";
+import Head from "next/head";
+import { getAllHakidamesData } from "@/lib/fetch";
+import { useRouter } from "next/router";
 
-export default function Index() {
-  console.log(SERVER_URL);
+export default function Home({ hakidames }: any) {
+  const router = useRouter();
+  console.log(router);
   return (
-    <div>
-      <div>hakidame</div>
-    </div>
+    <>
+      <Head>
+        <title>はきだめ | umaidashi</title>
+      </Head>
+      <HakidameList hakidames={hakidames} />
+    </>
   );
+}
+
+export async function getStaticProps() {
+  const res = await getAllHakidamesData();
+  const hakidames = res.data;
+  return {
+    props: { hakidames },
+    revalidate: 1,
+  };
 }
